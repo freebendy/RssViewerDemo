a47e8cad3f920480d0ee84b9c930ca8347c9a308
Ben Kwan <freebendy@gmail.com>
2011-04-12 14:19:54 +0800

    Download xml file in service and broadcast correctly.

--- src/com/roadmap/activity/StartUpActivity.java
+++ src/com/roadmap/activity/StartUpActivity.java
@@ -1,15 +1,84 @@
 package com.roadmap.activity;
 
 import com.roadmap.R;
+import com.roadmap.service.RSSDownLoadService;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class StartUpActivity extends Activity {
+    
+    private static final String LOG_TAG = "roadmap.StartUpActivity";
+    
+    private static final int DIALOG_DL_FAILED_ID = 0;
+    
+    public BroadcastReceiver mDownloadReceiver;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mDownloadReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.v(LOG_TAG, "mDownloadReceiver.onReceive");
+
+                if (intent.getAction().equals(RSSDownLoadService.ACTION_DOWMLOAD_SUCCESS)) {
+                    Toast.makeText(StartUpActivity.this, R.string.dl_finished, Toast.LENGTH_SHORT).show();
+                } else if (intent.getAction().equals(RSSDownLoadService.ACTION_DOWMLOAD_FAILED)) {
+                    Log.v(LOG_TAG, "mDownloadReceiver.onReceive: Failed");
+                    showDialog(DIALOG_DL_FAILED_ID);
+                }
+            }
+        };
+        
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(RSSDownLoadService.ACTION_DOWMLOAD_SUCCESS);
+        filter.addAction(RSSDownLoadService.ACTION_DOWMLOAD_FAILED);
+        registerReceiver(mDownloadReceiver, filter);
+        
+        Intent intent = new Intent(this, RSSDownLoadService.class);
+        startService(intent);
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(LOG_TAG, "onDestroy");
+        unregisterReceiver(mDownloadReceiver);
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Log.v(LOG_TAG, "onCreateDialog");
+        Dialog dialog = null;
+        switch (id) {
+        case DIALOG_DL_FAILED_ID:
+            AlertDialog.Builder builder = new AlertDialog.Builder(StartUpActivity.this);
+            builder.setMessage(R.string.dl_failed)
+                   .setCancelable(false)
+                   .setPositiveButton("Close", 
+                           new DialogInterface.OnClickListener() {
+                               public void onClick(DialogInterface dialog, int id) {
+                                   StartUpActivity.this.finish();
+                               }
+                   });
+            dialog = builder.create();
+            break;
+        default:
+            break;
+        }
+        return dialog;
     }
 }
--- src/com/roadmap/service/RSSDownLoadService.java
+++ src/com/roadmap/service/RSSDownLoadService.java
@@ -1,15 +1,70 @@
 package com.roadmap.service;
 
-import android.app.Service;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.app.IntentService;
 import android.content.Intent;
-import android.os.IBinder;
+import android.util.Log;
 
-public class RSSDownLoadService extends Service {
+public class RSSDownLoadService extends IntentService {
 
+    public static final String ACTION_DOWMLOAD_SUCCESS = "com.roadmap.service.ACTION_DOWMLOAD_SUCCESS";
+    public static final String ACTION_DOWMLOAD_FAILED = "com.roadmap.service.ACTION_DOWMLOAD_FAILED";
+    
+    private static final String LOG_TAG = "roadmap.RSSDownLoadService";
+    
+    private static final String RSS_URL_STRING = "http://rss.news.yahoo.com/rss/topstories";
+    
+    private String mRssContent = "";
+    
+    public RSSDownLoadService() {
+        super("RSSDownLoadService");
+    }
+    
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO Auto-generated method stub
-        return null;
+    protected void onHandleIntent(Intent intent) {
+        Log.v(LOG_TAG, "onHandleIntent");
+        
+        Intent broadcasetIntent = new Intent();
+        try {
+            URL url = new URL(RSS_URL_STRING);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            int reCode = connection.getResponseCode();
+            
+            if (HttpURLConnection.HTTP_OK == reCode)
+            {
+                InputStreamReader in = new InputStreamReader(connection.getInputStream());  
+                // Create the BufferedReader.
+                BufferedReader buffer = new BufferedReader(in);
+                mRssContent = "";
+                String inputLine = null;
+                // Get the data.
+                while (((inputLine = buffer.readLine()) != null))  
+                {  
+                    //我们在每一行后面加上一个"\n"来换行  
+                    mRssContent += inputLine;  
+                }
+                // Close InputStreamReader.
+                in.close();  
+                // Close http connection.
+                connection.disconnect();
+                
+                Log.v(LOG_TAG, mRssContent);
+                
+                broadcasetIntent.setAction(ACTION_DOWMLOAD_SUCCESS);
+            } else {
+                broadcasetIntent.setAction(ACTION_DOWMLOAD_FAILED);
+            }
+                
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            broadcasetIntent.setAction(ACTION_DOWMLOAD_FAILED);
+        }
+        sendBroadcast(broadcasetIntent);
     }
 
 }
