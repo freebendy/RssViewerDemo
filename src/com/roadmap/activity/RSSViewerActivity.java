2fcf33c31366f4a166743f2082cae0e753268127
BenKwan <freebendy@gmail.com>
2011-04-25 12:01:34 +0800

    Open feed in browser when item is clicked.

--- src/com/roadmap/activity/RSSViewerActivity.java
+++ src/com/roadmap/activity/RSSViewerActivity.java
@@ -4,17 +4,21 @@ import com.roadmap.NewsViewer.FeedColumns;
 import com.roadmap.R;
 
 import android.app.ListActivity;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 public class RSSViewerActivity extends ListActivity {
 
     private static final String LOG_TAG = "roadmap.RSSViewerActivity";
-    
+
     /**
      * The columns we are interested in from the database
      */
@@ -23,33 +27,45 @@ public class RSSViewerActivity extends ListActivity {
         FeedColumns.TITLE, // 1
         FeedColumns.IMAGEURL, // 2
     };
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.v(LOG_TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feeds_list);
-        
+
         // If no data was given in the intent (because we were started
         // as a MAIN activity), then use our default content provider.
         Intent intent = getIntent();
         if (intent.getData() == null) {
             intent.setData(FeedColumns.CONTENT_URI);
         }
-        
+
         // Perform a managed query. The Activity will handle closing and requerying the cursor
         // when needed.
-        Cursor cursor = managedQuery(getIntent().getData(), PROJECTION, null, null,
+        Cursor cursor = managedQuery(intent.getData(), PROJECTION, null, null,
                 FeedColumns.DEFAULT_SORT_ORDER);
 
-        // Used to map notes entries from the database to views
-//        SimpleCursorAdapter adapter = 
-//            new SimpleCursorAdapter(this, R.layout.feeds_row, cursor,
-//                new String[] { FeedColumns.TITLE }, new int[] { R.id.title });
-        
+        // Used to map rss entries from the database to views
+
         RSSListAdapter adapter = new RSSListAdapter(this, cursor);
-        
+
         setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Log.v(LOG_TAG, "onListItemClick - id: " + id);
+
+        Uri uri = ContentUris.withAppendedId(getIntent().getData(), id);
+        Cursor cursor = managedQuery(uri, new String[] {FeedColumns.LINK},
+                null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            String link = cursor.getString(cursor.getColumnIndex(FeedColumns.LINK));
+            Intent intent = new Intent("android.intent.action.VIEW",Uri.parse(link));
+            startActivity(intent);
+        }
     }
 
 }
